9aaed9d8ac7726222f139f2745fcf677d70374e0
Keren <keren.berelson@pearson.com>
2017-12-14 14:15:55 +0000

    first line drawn

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const boxSize = 100;
+
+const element = <svg 
+width={3 * boxSize} height={3 * boxSize}>
+<line x1="0" y1={boxSize} x2={3 * boxSize} y2={boxSize}
+  strokeWidth="3" stroke="black"/>
+</svg>
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(element, rootElement)
