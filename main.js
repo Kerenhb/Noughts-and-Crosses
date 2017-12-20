47f18bedefa6f3a9e8e2c34b3a9f1ab8153c2eaa
Keren <keren.berelson@pearson.com>
2017-12-20 11:56:45 +0000

    Added 'empty' form and app classes

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Game from './game'
+import Game from './game';
+import Form from './form';
 
-const element = <Game />;
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <Game />
+        )
+    };
+}
+
+const element = <App />;
 const rootElement = document.getElementById('root');
 ReactDOM.render(element, rootElement)
