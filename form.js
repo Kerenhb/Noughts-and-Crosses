ebd0b53f116b527f576ad83447fd3e2f948ce4c1
Keren <keren.berelson@pearson.com>
2017-12-20 12:24:21 +0000

    Added names to form

--- form.js
+++ form.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
 export default class Form extends React.Component {
+    render() {
+        return(
+            <div>
+                <h3>Player 1</h3>
+                Name: <input type="text" onChange = {event => {this.props.updateName(event, 0)}}/>
+
+                <h3>Player 2</h3>
+                Name: <input type="text" onChange = {event => {this.props.updateName(event, 1)}}/>
+            </div>
+        )
+    }
+    // Choose color
+    // Choose name
+    // Swap who's X (and therefore starts) each time there's a new game (toggable?)
 
 }
--- main.js
+++ main.js
@@ -4,10 +4,29 @@ import Game from './game';
 import Form from './form';
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            playerNames: ['Player 1', 'Player 2'],
+            playerColors: ['red', 'blue'],
+            player1Cross: true, // Player 1 is X and therefore starts
+        };
+
+        this.updateName = this.updateName.bind(this);
+    }
+
+    updateName(event, playerNumber){
+        let playerNames = this.state.playerNames;
+        playerNames[playerNumber] = event.target.value;
+        this.setState({playerNames});
+    }
 
     render() {
         return (
-            <Game />
+            <Form
+                updateName = {this.updateName}
+            />
         )
     };
 }
