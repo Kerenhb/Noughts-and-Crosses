18346820a040cf7aca32deeb5af8532709bdf45f
Keren <keren.berelson@pearson.com>
2017-12-21 10:18:29 +0000

    Better layout for the form using tables

--- form.js
+++ form.js
@@ -6,25 +6,49 @@ export default class Form extends React.Component {
             <div>
                 <h2>Game Settings</h2>
                 <h3>Player 1 (<span style={{"color":this.props.playerColors[0]}}>x</span>)</h3>
-                Name: <input type="text" value = {this.props.playerNames[0]}
-                onChange = {event => {this.props.updateName(event, 0)}}/>
-                <br />
-                Color: <input type="color" value = {this.props.playerColors[0]}
-                onChange = {event => {this.props.updateColor(event, 0)}}/>
+                <table><tbody>
+                    <tr><td>Name:</td>
+                    <td>
+                        <input type="text" value = {this.props.playerNames[0]}
+                        onChange = {event => {this.props.updateName(event, 0)}}/>
+                    </td></tr>
+
+                    <tr><td>Color:</td>
+                    <td>
+                        <input type="color" value = {this.props.playerColors[0]}
+                        onChange = {event => {this.props.updateColor(event, 0)}}/>
+                    </td></tr>
+                </tbody></table>
 
                 <h3>Player 2 (<span style={{"color":this.props.playerColors[1]}}>o</span>)</h3>
-                Name: <input type="text" value = {this.props.playerNames[1]}
-                onChange = {event => {this.props.updateName(event, 1)}}/>
-                <br />
-                Color: <input type="color" value = {this.props.playerColors[1]}
-                onChange = {event => {this.props.updateColor(event, 1)}}/>
+                <table><tbody>
+                    <tr><td>Name:</td>
+                    <td>
+                        <input type="text" value = {this.props.playerNames[1]}
+                        onChange = {event => {this.props.updateName(event, 1)}}/>
+                    </td></tr>
+
+                    <tr><td>Color:</td>
+                    <td>
+                        <input type="color" value = {this.props.playerColors[1]}
+                        onChange = {event => {this.props.updateColor(event, 1)}}/>
+                    </td></tr>
+                </tbody></table>
 
                 <h3>Misc Settings</h3>
-                Grid size: <input type="number" value = {this.props.gridSize}
-                onChange = {event => {this.props.updateGridSize(event)}} />
-                <br />
-                Number of games to play: <input type="number" value = {this.props.numberOfGames}
-                onChange = {event => {this.props.updateNumberOfGames(event)}} />
+                <table><tbody>
+                    <tr><td>Grid Size:</td>
+                    <td>
+                        <input type="number" value = {this.props.gridSize}
+                        onChange = {event => {this.props.updateGridSize(event)}} />
+                    </td></tr>
+
+                    <tr><td> Number of games to play:</td>
+                    <td>
+                        <input type="number" value = {this.props.numberOfGames}
+                        onChange = {event => {this.props.updateNumberOfGames(event)}} />
+                    </td></tr>
+                </tbody></table>
 
                 <h4>Who starts?</h4>
                 Player 1: <input type = 'radio' name = "start" checked = {this.props.player1starts}
